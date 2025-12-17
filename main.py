949ad97a9a1bff467071752ed32d29d0d3edf797
Rasif <rasifrazak123@gmail.com>
2025-12-17 23:05:56 +0530

    add two linklists

--- main.py
+++ main.py
@@ -364,3 +364,27 @@ c.next = d
 link = SingleLinkedlist(a)
 print(link.reverse(a))
 print(link.printlist()) 
+
+
+def addTwoNumbers(l1: Node, l2: Node) -> Node:
+        dummy = Node(0,0)
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            total = val1 + val2 + carry
+
+            carry = total // 10
+            current.next = Node(total % 10,None)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
+    
+print(addTwoNumbers(a,a))
